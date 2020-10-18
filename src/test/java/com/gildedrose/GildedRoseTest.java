92d81fe547b55f4b3781f86f9f7877c4d4377c2e
rosarkis1 <71924855+rosarkis1@users.noreply.github.com>
2020-10-18 11:48:56 +0200

    V1.3

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,19 +11,33 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0),
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("+5 Dexterity Vest", -10, 20),
+                new Item("+5 Dexterity Vest", -10, -20),
+                new Item("+5 Dexterity Vest", -10, 60),
                 new Item("Aged Brie", 2, 0), //
                 new Item("Aged Brie", -2, 48),
+                new Item("Aged Brie", 5,-5),
+                new Item("Aged Brie", 5,51),
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Sulfuras, Hand of Ragnaros", 1, 80),
+                new Item("Sulfuras, Hand of Ragnaros", 1, 70),
+                new Item("Sulfuras, Hand of Ragnaros", -1, -80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25),
-                new Item("Backstage passes to a TAFKAL80ETC concert",  -5,0),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -5,0),
+                new Item("Backstage passes to a TAFKAL80ETC concert",10,3),
+                new Item("Backstage passes to a TAFKAL80ETC concert",11,3),
+                new Item("Backstage passes to a TAFKAL80ETC concert",5,3),
+                new Item("Backstage passes to a TAFKAL80ETC concert",5,48),
+                new Item("Backstage passes to a TAFKAL80ETC concert",-1,3),
+                new Item("Backstage passes to a TAFKAL80ETC concert",10,-3),
+                new Item("Backstage passes to a TAFKAL80ETC concert",10,51),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6),
                 new Item("Conjured Mana Cake", -3, 20),
+                new Item("Conjured Mana Cake", 3, -6),
+                new Item("Conjured Mana Cake", 3, 51)
         };
 
         GildedRose app = new GildedRose(items);
@@ -34,6 +48,7 @@ class GildedRoseTest {
         }
         app.items[0].toString();
         assertThat(app.items[0].name, is("foo"));
+
 
 
 
